86b6fd55af5857b8f7399fd3b3f76ba81d81138b
David-F-98 <u2903149@unimilitar.edu.co>
2024-01-09 12:46:58 -0500

    Validando Input de Precio

--- public/bundle.js
+++ public/bundle.js
@@ -24,8 +24,10 @@ botonFormulario.addEventListener('click',(e)=>{
 const formulario = document.querySelector('#formulario-gasto form');
 //Accedemos al id=descripcion mediante esta otra manera
 const descripcion = formulario.descripcion;
+const precio =  formulario.precio;
 
 const expRegDescripcion = /^[a-zA-Z0-9\_\- ]{4,30}$/;
+const expRegPrecio = /^\d+(\.\d+)?$/;
 
 const comprobarDescripcion = ()=>{
     if(!expRegDescripcion.test(descripcion.value)){
@@ -45,18 +47,49 @@ const comprobarDescripcion = ()=>{
             .classList.remove('formulario-gasto__leyenda--active');
 
         return true;
-    }
-};
+    }};
+
+const comprobarPrecio = ()=>{
+    if(!expRegPrecio.test(precio.value)){
+        precio.classList.add('formulario-gasto__input--error');
+
+        //Nos dirijimos al elemento padre y buscamos la clase .formulario-gasto__leyenda para añadirle o modificarlo
+        formulario.precio.parentElement
+            .querySelector('.formulario-gasto__leyenda')
+            .classList.add('formulario-gasto__leyenda--active');
+
+        return false;
+    }else {
+        precio.classList.remove('formulario-gasto__input--error');
+
+        formulario.precio.parentElement
+            .querySelector('.formulario-gasto__leyenda')
+            .classList.remove('formulario-gasto__leyenda--active');
+
+        return true;
+    }};
+
 
 //comprobando al hacer click fuera del input
 descripcion.addEventListener('blur', (e)=>comprobarDescripcion());
-
 //Comprobando al teclear en el input
 descripcion.addEventListener('keyup', (e)=>{
 //Trasformamos el elmento que obtenemos en  e.target.classList en una arreglo para saber si tienen la respectiva clase;
-const  validacionAlTeclear =   [...e.target.classList].includes('formulario-gasto__input--error');
-    if(validacionAlTeclear){
+const  validacionAlTeclearDes =   [...e.target.classList].includes('formulario-gasto__input--error');
+    if(validacionAlTeclearDes){
         comprobarDescripcion();
+    }
+});
+
+
+//comprobando al hacer click fuera del input
+precio.addEventListener('blur', (e)=>comprobarPrecio());
+//Comprobando al teclear en el input
+precio.addEventListener('keyup', (e)=>{
+//Trasformamos el elmento que obtenemos en  e.target.classList en una arreglo para saber si tienen la respectiva clase;
+const  validacionAlTeclearPre =   [...e.target.classList].includes('formulario-gasto__input--error');
+    if(validacionAlTeclearPre){
+        comprobarPrecio();
     }
 });
 //# sourceMappingURL=bundle.js.map
