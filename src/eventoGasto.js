d46f669cb8acd573f4d3518c69679b5b7653aebf
David-F-98 <u2903149@unimilitar.edu.co>
2024-01-13 11:00:15 -0500

    Evento editar Gasto

--- src/eventoGasto.js
+++ src/eventoGasto.js
@@ -1,8 +1,12 @@
+import { stringify } from "uuid";
+import { abrirFormularioDesdeGastos } from "./eventoBtnFormularioGastos";
+
 const contenedorGastos = document.getElementById('gastos');
 
 contenedorGastos.addEventListener('click', (e)=>{
     e.preventDefault();
     const gasto = e.target.closest('.gasto');
+
     if(gasto){
         if(gasto.scrollLeft > 0){
             gasto.querySelector('.gasto__info').scrollIntoView({
@@ -18,5 +22,29 @@ contenedorGastos.addEventListener('click', (e)=>{
             });
 
         };
-    }
+    };
+
+    if(e.target.closest('[data-accion="editar-gasto"]')){
+        const id = gasto.dataset.id;
+        
+        const gastosGuardados = JSON.parse(window.localStorage.getItem('gastos'));
+
+        let precio = '';
+        let  descripcion = '';
+    
+        if(gastosGuardados && gastosGuardados.length>0){
+            gastosGuardados.forEach((gasto) => {
+                if(id === gasto.id){
+                    precio = gasto.precio;
+                    descripcion = gasto.descripcion;
+                };
+            });
+            
+            document.querySelector('#formulario-gasto #descripcion').value = descripcion;
+            document.querySelector('#formulario-gasto #precio').value = precio;
+            document.querySelector('#formulario-gasto').dataset.id = id;
+            abrirFormularioDesdeGastos('editarGasto');
+        };
+
+    };
 })
